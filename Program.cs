66e3c03c83aaef68364505cbde5076b9afb45e2c
Tygrak <S-KlepacD@seznam.cz>
2017-02-11 23:11:49 +0100

    For loop, % operator and copying variables added

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace ConsoleApplication{
     public class Program{
         public static void Main(string[] args){
             string path = Directory.GetCurrentDirectory()+"\\toCompile.txt";
+            if(args.Length > 0){
+                for (int i = 0; i < args.Length; i++){
+                    Console.WriteLine(args[i]);
+                }
+                path = Directory.GetCurrentDirectory()+"\\"+args[0];
+            }
             var inputText = File.ReadAllLines(path);
             tildeProgram prog = new tildeProgram(inputText);
             prog.compileProgram();
@@ -14,7 +20,7 @@ namespace ConsoleApplication{
 
     public class tildeVar{
         public string name = "";
-        public string tildeVal = "";
+        private string tildeVal = "";
         private int val = 0;
         public tildeVar(string name, string tildeValue){
             this.name = name;
@@ -48,11 +54,14 @@ namespace ConsoleApplication{
     public class tildeProgram{
         public string[] lines;
         public List<tildeVar> variables = new List<tildeVar>();
+        public List<int[]> loops = new List<int[]>();
         private string currentString = "";
         private string lastCommand = "";
         private int startDepth = 0;
         private int depth = 0;
         private int readMode = 0;
+        private int i = 0;
+        private int j = 0;
         private List<string> arguments = new List<string>();
 
         public tildeProgram(string[] lines){
@@ -61,10 +70,11 @@ namespace ConsoleApplication{
 
         public void compileProgram(){
             currentString = "";
-            for (int i = 0; i<lines.Length; i++){
+            for (i = 0; i<lines.Length; i++){
                 string line = lines[i];
-                Console.WriteLine(line);
-                for (int j = 0; j<line.Length; j++){
+                line = line.Split(new string[] {"//"}, StringSplitOptions.None)[0];
+                //Console.WriteLine(line);
+                for (j = 0; j<line.Length; j++){
                     char c = line[j];
                     string character = c.ToString();
                     string nextCharacter = "";
@@ -84,6 +94,11 @@ namespace ConsoleApplication{
                             if(j+4 < line.Length && line.Substring(j, 5) == "-----"){
                                 j += 4;
                                 depth -= 1;
+                                if(loops.Count != 0 && loops[loops.Count-1][2] == depth){
+                                    i = loops[loops.Count-1][0]-1;
+                                    j = loops[loops.Count-1][1];
+                                    break;
+                                }
                                 continue;
                             } else if(j+3 < line.Length && line.Substring(j, 4) == "----"){
                                 j += 3;
@@ -149,6 +164,13 @@ namespace ConsoleApplication{
             } else if(currentString == "~~~"){ //If : arg1=mode, arg2=var1, arg3=var2
                 readMode = 1;
                 arguments.Clear();
+            } else if(currentString == "~~~~"){ //For : arg1=mode, arg2=var1, arg3=var2
+                int[] loopPos = {i, j-3, depth, 0};
+                if(loops.FindIndex(x => x[0].Equals(i) && x[1].Equals(j-3)) == -1){
+                    loops.Add(loopPos);
+                }
+                readMode = 1;
+                arguments.Clear();
             } else if(currentString == "~~~~~"){ //Print as int or char : arg1=mode, arg2=var
                 readMode = 1;
                 arguments.Clear();
@@ -167,6 +189,11 @@ namespace ConsoleApplication{
                     readMode = 0;
                 }
             } else if(lastCommand == "~~~"){
+                arguments.Add(currentString);
+                if(arguments.Count == 3){
+                    readMode = 0;
+                }
+            } else if(lastCommand == "~~~~"){
                 arguments.Add(currentString);
                 if(arguments.Count == 3){
                     readMode = 0;
@@ -206,6 +233,12 @@ namespace ConsoleApplication{
                     } else if(arg0 == "~~~~"){
                         tildeVar v2 = variables.Find(x => x.name.Equals(arguments[2]));
                         v1.value = v1.value/v2.value;
+                    } else if(arg0 == "~~~~~"){
+                        tildeVar v2 = variables.Find(x => x.name.Equals(arguments[2]));
+                        v1.value = v1.value%v2.value;
+                    } else if(arg0 == "~~~~~~"){
+                        tildeVar v2 = variables.Find(x => x.name.Equals(arguments[2]));
+                        v1.value = v2.value;
                     } else if(arg0 == "~-~"){
                         v1.value = v1.value+tildeVar.getTildeValue(arguments[2]);
                     } else if(arg0 == "~-~~"){
@@ -214,6 +247,8 @@ namespace ConsoleApplication{
                         v1.value = v1.value*tildeVar.getTildeValue(arguments[2]);
                     } else if(arg0 == "~-~~~~"){
                         v1.value = v1.value/tildeVar.getTildeValue(arguments[2]);
+                    } else if(arg0 == "~-~~~~~"){
+                        v1.value = v1.value%tildeVar.getTildeValue(arguments[2]);
                     } 
                 }
             } else if(lastCommand == "~~~"){
@@ -272,6 +307,56 @@ namespace ConsoleApplication{
                             depth+=1;
                         } else{
                             readMode = 2; startDepth = depth; depth+=1;
+                        }
+                    }
+                }
+            } else if(lastCommand == "~~~~"){
+                if(arguments.Count == 3){
+                    loops[loops.Count-1][3] += 1;
+                    tildeVar v1 = variables.Find(x => x.name.Equals(arguments[1]));
+                    string arg0 = arguments[0];
+                    if(arg0 == "~"){
+                        if(loops[loops.Count-1][3] == 1){
+                            v1.value -= 1;
+                        }
+                        v1.value += 1;
+                        tildeVar v2 = variables.Find(x => x.name.Equals(arguments[2]));
+                        if(v1.value<v2.value){
+                            depth+=1;
+                        } else{
+                            readMode = 2; startDepth = depth; depth+=1; loops.RemoveAt(loops.Count-1);
+                        }
+                    } else if(arg0 == "~~"){
+                        if(loops[loops.Count-1][3] == 1){
+                            v1.value += 1;
+                        }
+                        v1.value -= 1;
+                        tildeVar v2 = variables.Find(x => x.name.Equals(arguments[2]));
+                        if(v1.value>v2.value){
+                            depth+=1;
+                        } else{
+                            readMode = 2; startDepth = depth; depth+=1; loops.RemoveAt(loops.Count-1);
+                        }
+                    } else if(arg0 == "~-~"){
+                        if(loops[loops.Count-1][3] == 1){
+                            v1.value -= 1;
+                        }
+                        v1.value += 1;
+                        if(v1.value<tildeVar.getTildeValue(arguments[2])){
+                            depth+=1;
+                        } else{
+                            readMode = 2; startDepth = depth; depth+=1; loops.RemoveAt(loops.Count-1);
+                        }
+                    } else if(arg0 == "~-~~"){
+                        if(loops[loops.Count-1][3] == 1){
+                            v1.value += 1;
+                        }
+                        v1.value -= 1;
+                        int a = tildeVar.getTildeValue(arguments[2]);
+                        if(v1.value>tildeVar.getTildeValue(arguments[2])){
+                            depth+=1;
+                        } else{
+                            readMode = 2; startDepth = depth; depth+=1; loops.RemoveAt(loops.Count-1);
                         }
                     }
                 }
